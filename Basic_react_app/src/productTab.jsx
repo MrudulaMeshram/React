2e04abbe607ff5f07542b952265fd1b953b0e789
Mrudula Meshram <mrudulameshram138@gmail.com>
2026-04-11 17:36:35 +0530

    initial commit

---
+++ Basic_react_app/src/productTab.jsx
@@ -0,0 +1,13 @@
+import product from "./product.jsx";
+
+function ProductTab() {
+    return (
+     <>
+     <product />
+     <product />
+     <product />
+     </>
+    );
+}
+
+export default ProductTab;
